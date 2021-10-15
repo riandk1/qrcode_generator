45f92fd9dbf931b0fb8171c41e25bf2ae2546bd8
Rian de Almeida <rian223@outlook.com.br>
2021-10-14 22:43:03 -0400

    add workflow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import QRCodeCanvas from './QRCodeCanvas';
 
 function App() {
+
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={(element) => (setText(element.target.value))} value={text}></input>
+      <QRCodeCanvas text={text}></QRCodeCanvas>
     </div>
   );
 }
